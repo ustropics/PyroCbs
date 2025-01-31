d4b662d53e7c08dd84950ad31cdd97022598d4f6
USTropics <ustropics@gmail.com>
2025-01-31 15:46:05 -0500

    update to database

--- convert_sample.py
+++ convert_sample.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image, UnidentifiedImageError
 
-fire_id = 'A05'
+fire_id = 'A09'
 
 def resize_images(input_dir, output_dir):
     # Create the output directory if it doesn't exist
